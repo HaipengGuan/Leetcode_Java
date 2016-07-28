1a98e740d56fe8415df49d2f40003603f11f52e0
HaipengGuan <hpguan@freewheel.tv>
2016-07-28 10:20:10 +0800

    140. Word Break II: improve DP

--- backtracking/wordbreak/WordBreakII.java
+++ backtracking/wordbreak/WordBreakII.java
@@ -30,31 +30,30 @@ public class WordBreakII {
     	List<String> res = new ArrayList<>();
     	if (s.length() == 0 || wordDict.size() == 0) return res;
     	int n = s.length();
-    	boolean[] dp = new boolean[n+1];
-    	boolean[][] dp2 = new boolean[n][n];
-    	dp[0] = true;
-    	for (int i = 1; i <= n; i++) {
-			for (int j = i-1; j >= 0; j--) {
-				if (dp[j] && wordDict.contains(s.substring(j, i))) {
-					dp[i] = true;
-					dp2[j][i-1] = true;
+    	boolean[] possible = new boolean[n+1];
+    	boolean[][] dp = new boolean[n][n];
+    	int[] lastInds = new int[n];
+    	possible[0] = true;
+    	for (int end = 1; end <= n; end++) {
+			for (int begin = 0; begin < end; begin++) {
+				if (possible[begin] && wordDict.contains(s.substring(begin, end))){
+					possible[end] = true;
+					dp[begin][end-1] = true;
+					lastInds[begin] = end-1;
 				}
 			}
 		}
-    	if (dp[n] != false) dfs(s, dp2, 0, res, new StringBuffer(2*n));
+    	
+    	if (possible[n]) dfs(s, 0, dp, lastInds, res, "");
     	return res;
-    }
-    
-    private void dfs(String s, boolean[][] dp2, int begin, List<String> res, StringBuffer buffer) {
+	}
+	
+	private void dfs(String s, int begin, boolean[][] dp, int[] lastInds, List<String> res, String cur) {
 		if (begin >= s.length()) {
-			res.add(buffer.toString().substring(0, buffer.length()-1));
+			res.add(cur.substring(0, cur.length()-1));
 		} else {
-			for (int end = begin; end < s.length(); end++) {
-				if (!dp2[begin][end]) continue;
-				String word = s.substring(begin, end+1);
-				buffer.append(word + " ");
-				dfs(s, dp2, end+1, res, buffer);
-				buffer.delete(buffer.length()-word.length()-1, buffer.length());
+			for (int end = begin; end <= lastInds[begin]; end++) {
+				if (dp[begin][end]) dfs(s, end+1, dp, lastInds, res, cur + s.substring(begin, end+1) + " ");
 			}
 		}
 	}
@@ -69,6 +68,7 @@ public class WordBreakII {
 		for (String string : res) {
 			System.out.println(string);
 		}
+		System.out.println(res.size());
 		System.out.println("----------");
 	}
     
@@ -76,7 +76,7 @@ public class WordBreakII {
     	WordBreakII word = new HashMapApproach();
     	test(word, "catsanddog", new String[] {"cat", "cats", "and", "sand", "dog"});
     	test(word, "bbbbbbbb", new String[] {"cat", "cats", "and", "sand", "dog"});
-    	test(word, "aaaaaaaaaaaa", new String[] {"aa", "aaa"});
+    	test(word, "aaaaaaaaaaaaaaaaaa", new String[] {"aa", "aaa"});
 	}
 }
 
